4f678c6575b4ef945a0525fd4e9c099335e433a4
rashidkhan <rashidkhanrk5456@email.com>
2023-02-15 16:07:31 +0530

    javascript-test

---
+++ test-2 js/myscript.js
@@ -0,0 +1,167 @@
+// 1 Take two numbers and take a value which should be the sum of the two numbers plus some other number. Now findout the 3rd number.
+
+// 2 Ask the below questions:
+
+// What is your English marks in 10th?
+// What is your Science marks in 10th?
+// What is your Math marks in 10th?
+// What is your Hindi marks in 10th?
+// What is your SST marks in 10th? Now calculate percentage by taking all the 5 subjects numbers.
+
+// 3 In which century were your born? find out by asking birth year. Use Math.floor function to remove decimal portion of the century.
+
+// 4 Add 20 in your name and check value in console. Add true to your name and check output Add 30+"Wecode" and check type of the final value.
+
+// 5 Create Reverse Game. Ask any question with 2 possible answers. If user selects first option then show second option in alert and vice versa.
+
+// For example: Do you drink coffee or tea? and If user says coffee then show Tea in alert and if user says tea then show coffee.
+
+// 6 Test below conditions using &&, || and not logical operators. Take any number check if number is even and also check if number is multiple of 4 or not. If both conditions are true then check if number is less than 100 or number is 84 or not.
+
+// 7 Take city temprature. If temp is less than 10 degree then print very cold. If city temp is less than 30 and greater than equal to 10 then print cold. If temp is between 30 to 40 then print Hot. Else print Very hot
+
+// 8 Using prompt ask user about his/her age. If user is greater than 18 then show user message "Do you want to open account?" If yes click on Okay button then ask your 4 below questions
+
+// What is your name ?
+// What is your aaadhar number ?
+// What is your address?
+// What is your mobile number? and print all these details in console. If click on cancel then show message "Sorry, please visit again."
+
+// 9 Create a simple calcultor. Firslty ask your about the operation. Like "What do you want to do?" User can type choice in any way like +,- or plus, minus or add, sub etc. After that ask user about first number and then second number and finally show the desired output.
+
+// 10 Who is most intelligent in your family? Ask 10th percentage of each family member and check who got the highest marks. If total family members are more than 4 then you can use Math.max() function
+
+// ans -4
+
+// let name ="rashid";
+// let a =20;
+// let b=30;
+// let c="wecode";
+// console.log(name+a);
+
+// console.log(name+true);
+// console.log(b+ c);
+
+// ans-5
+
+// let y=11;
+// let z=12;
+
+// if(y*2==22 && z*2==25){
+//     console.log("yes");
+// }
+// else{
+//     alert("no");
+// }
+
+// // ans-7
+
+// let tab =15;
+
+// if(tab>20){
+//     console.log("very cool");
+
+// }
+
+// else{
+//     console.log("cool");
+// }
+
+//    ans-1
+
+// let s=10;
+// let r=25;
+// let total=60;
+// let t=(total-(s+r));
+// console.log(t)
+
+// // ans-3
+
+// let century =2002;
+// let year= century/100;
+// console.log(Math.floor(year));
+
+// 1 Create two variables and store some numbers in them. Now do below operations:
+
+// Addition
+// Multiplication
+// Substraction
+// Divide
+
+// 2 Take one number. If number is odd then print odd else print even.
+
+// 3 Using prompt ask user to enter name and then show the name in alert.
+
+// 4 Take two numbers if first number is less than second number than print your name, age and mobile number else print your father name, age and mobile number.
+
+//  5 Take two numbers. Now using && and || operator check if both numbers are multiple of 5 && 10 or not. Also check both numbers are even or not.
+
+//  6print Hello world using console.log, window.alert and document.write
+
+// 7 Take all falsy values and check in if condition.
+
+// 8 Using confirm box ask questions "Do you want to play?". if user click on Ok then print yes and if user click on Cancel then print No
+
+// 9 If total members in your family are more than 5 then print Big Family, if members are between 3 to 5 then print small family else print very small family
+
+// 10 Using ++, -- print 1 to 10 and then 10 to 5
+
+// ans-1
+
+// let a = 15;
+// let b = 10;
+
+// console.log(a + b);
+// console.log(a * b);
+// console.log(a - b);
+// console.log(a / b);
+
+// ans-2
+
+// let c = 4;
+// let d = 5;
+
+// if (c % 2 === 0 && d % 2 === 0) {
+//   console.log("even");
+// } else {
+//   console.log("odd");
+// }
+
+// ans-3
+
+/*
+let name="mukesh"
+prompt(name);
+alert(name);
+*/
+
+// // ans-4
+
+// let y = 18;
+// let x = 21;
+// let name = "rashid";
+// let age = 21;
+// let mobile = 232544343;
+
+// if (y < x) {
+//   console.log(name, age, mobile);
+// } else {
+//   console.log("no");
+// }
+
+
+
+// // ans-5
+
+// let  r=20;
+// let s=32;
+
+// if(r%5===0 &&  s%5===0 && r%10===0 && s%10===0 ){
+//     console.log("even")
+
+// }
+// else{
+//     console.log("odd")
+// }
+
+
